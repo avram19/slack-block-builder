c076282a7a14e320201708808831b6f091d230b0
avram19 <avinash.ramesha@springworks.in>
2022-08-21 12:04:26 +0530

    Exported all types for usage

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import Blocks from './blocks';
 import Composition from './composition';
 import Elements from './elements';
 import Views from './views';
+import * as Types from './interfaces';
 
 export default {
   blocks: new Blocks(),
   elements: new Elements(),
   composition: new Composition(),
   views: new Views(),
+  Types,
 };
